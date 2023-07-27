cff56a73077d597c8602cb91a5f3cb82c6ec89c3
kunal66666 <kunal.kashyap.fiem.cse19@teamfuture.in>
2023-07-27 11:36:27 +0530

    react liefecycle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
 import './App.css';
-
+import { useState } from 'react';
+import  Text  from './Text';
 function App() {
+  const [showText,setShowText]=useState(false);
   return (
     <div className="App">
-      <h1 style={{color:"red"}}>my is kashyap</h1>
+      <button 
+      onClick={()=>{
+           setShowText(!showText);
+      }}
+      >
+        Show Text
+      </button>
+      {showText && <Text/>}
     </div>
    
   );
 }
 
 export default App;
+
+//mouting  appearing on the screen
+//updating component is changing
+//unmountiing   disappearing on the screen
