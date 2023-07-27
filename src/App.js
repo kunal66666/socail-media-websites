180109ef7b38fd3aec7843e66f07b0c41f178f3b
kunal66666 <kunal.kashyap.fiem.cse19@teamfuture.in>
2023-07-27 10:48:31 +0530

    main public

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color:"red"}}>my name is kashyap</h1>
+      <h1 style={{color:"red"}}>my is kashyap</h1>
     </div>
    
   );
